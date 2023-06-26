67e568c636cfa5d55320407613e305894f002d57
amanuel12345a <am@nuelsew@gegn@gmail.com>
2023-06-25 19:34:24 -0500

    edit

--- server.js
+++ server.js
@@ -4,12 +4,16 @@ const nodemailer = require('nodemailer')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const {testmony} = require('./control/schema')
+const { images } = require('./control/Images')
 require("dotenv").config()
 app.use(cors())
-app.use(express.json())
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3002
+
+mongoose.connect(`mongodb+srv://amnuelsewgegn:${process.env.MONGODBPASSWORD}@cluster0.m3q9i77.mongodb.net/photoretryWrites=true&w=majority`)
 app.post('/email',(req,res)=>{
     const {firstName,email,text,lastName, phone} = req.body
     var transporter = nodemailer.createTransport({
@@ -54,6 +58,25 @@ app.post('/data',async (req,res)=>{
 app.get('/data',async (req,res)=>{
     const responce = await Data.find({})
     res.send(responce)
+})
+const img = mongoose.model('image',images)
+app.post('/image',async (req,res)=>{
+  const {image,imageType} = req.body
+  const singleImg = new img({
+    image,
+    imageType
+  })
+  await singleImg.save().then(()=>console.log('working1'))
+  res.send('data recieved')
+})
+app.get('/image',async(req,res)=>{
+  let images = await img.find({})
+  res.send(images)
+})
+app.delete('/image/:id',async(req,res)=>{
+  const {id} = req.params
+  img.findOneAndRemove({_id:id}).then(()=>console.log('deleted'))
+  res.send('image deleted')
 })
 app.listen(PORT,()=>{
     console.log("working")
